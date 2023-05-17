4b569cf595cdcf925a31fb16efe6c420bf7bed7b
jayden-n <jaydennguyen.dev@gmail.com>
2023-05-17 16:27:01 -0400

    added row functionality

--- src/components/Row.jsx
+++ src/components/Row.jsx
@@ -1,7 +1,9 @@
 import React, { Fragment, useEffect, useState } from 'react';
-import { FaHeart, FaRegHeart } from 'react-icons/fa';
+import { MdChevronLeft, MdChevronRight, MdchevronRight } from 'react-icons/md';
 import axios from 'axios';
-const Row = ({ title, fetchURL }) => {
+import Movie from './Movie';
+
+const Row = ({ title, fetchURL, rowID }) => {
   const [movies, setMovies] = useState([]);
 
   useEffect(() => {
@@ -10,31 +12,37 @@ const Row = ({ title, fetchURL }) => {
     });
   }, [fetchURL]);
 
-  console.log(movies);
+  const slideLeft = () => {
+    var slider = document.getElementById('slider' + rowID);
+    slider.scrollLeft = slider.scrollLeft - 500;
+  };
+  const slideRight = () => {
+    var slider = document.getElementById('slider' + rowID);
+    slider.scrollLeft = slider.scrollLeft + 500;
+  };
 
   return (
     <Fragment>
       <h2 className="text-white font-bold md:text-xl p-4">{title}</h2>
-      <div className="relative flex items-center">
-        <div id={'slider'}>
+      <div className="relative flex items-center group">
+        <MdChevronLeft
+          onClick={slideLeft}
+          className="bg-white left-0 rounded-full absolute opacity-50 hover:opacity-100 cursor-pointer z-10 hidden group-hover:block"
+          size={40}
+        />
+        <div
+          id={'slider'+ rowID}
+          className="w-full h-full overflow-x-scroll whitespace-nowrap scroll-smooth scrollbar-hide relative"
+        >
           {movies.map((item, id) => (
-            <div className="w-[160px] sm:w-[200px] md:w-[240px] lg:w-[280px] inline-block cursor-pointer relative p-2">
-              <img
-                className="w-full h-auto block"
-                src={`https://image.tmdb.org/t/p/w500/${
-                  item && item.backdrop_path
-                }`}
-                alt={item.title}
-              />
-
-              <div className="absolute top-0 left-0 w-full h-full hover:bg-black/80 opacity-0 hover:opacity-100 text-white">
-                <p className="white-space-normal text-xs md:text-sm font-bold flex justify-center items-center h-full text-center">
-                  {item && item.title}
-                </p>
-              </div>
-            </div>
+            <Movie key={id} item={item} />
           ))}
         </div>
+        <MdChevronRight
+          onClick={slideRight}
+          className="bg-white right-0 rounded-full absolute opacity-50 hover:opacity-100 cursor-pointer z-10 hidden group-hover:block"
+          size={40}
+        />
       </div>
     </Fragment>
   );
