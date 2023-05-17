a7df3c112cde8e17522dfb95216987edd04baf9c
jayden-n <jaydennguyen.dev@gmail.com>
2023-05-17 00:54:10 -0400

    ui changes

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import React, { Fragment } from 'react';
 import NavBar from './components/NavBar';
+import Home from './pages/Home';
+import { Route, Routes } from 'react-router-dom';
 
 function App() {
   return (
     <Fragment>
-      <NavBar/>
+      <NavBar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
     </Fragment>
   );
 }
